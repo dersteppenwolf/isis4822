6856963d2d05317e044ca8c35d938dfe964aa7ce
juanmendez@gkudos.com <juanmendez@gkudos.com>
2018-10-18 21:33:17 -0500

    filter by locality

--- hw4/js/homeController.js
+++ hw4/js/homeController.js
@@ -2,44 +2,34 @@
 dataViz.controller('homeController', function (
   $scope, $interval, $rootScope, $http, $log, $filter) {
 
+  $scope.trends = []
+  $scope.localities = []
+  $scope.selectedLocality = ""
+
   $scope.remoteServiceUrl = "https://kudosg.carto.com/api/v2/sql?q="
+
+  $scope.parseDate = d3.timeParse("%Y-%m-%d");
+  $scope.parseYear = d3.timeParse("%Y");
 
   $scope.slider = {
     value: 1,
     options: {
       floor: 1,
       ceil: 30,
-      onEnd: function(id, newValue, highValue, pointerType) {
+      onEnd: function (id, newValue, highValue, pointerType) {
         $log.log('change', id, newValue, pointerType)
         $scope.loadTrends();
       }
     },
   }
 
-
-  $scope.parseDate = d3.timeParse("%Y-%m-%d");
-  $scope.parseYear = d3.timeParse("%Y");
-
-
-    /*
-  $scope.loadData = function () {
-    $log.log("loadData - homeController");
-
-    var dataUrl = "data/defunciones_year.TSV";
-    $scope.datasetYear = d3.tsv(dataUrl)
-      .then($scope.parseData)
-      .then((a) => {
-        $scope.maxScaleRef = d3.max(a, d => d["count"]);
-        console.log($scope.maxScaleRef);
-        return a;
-      });
-
-    var dataUrl = "data/defunciones_year_sex.TSV";
-    $scope.datasetYearSex = d3.tsv(dataUrl)
-      .then($scope.parseData)
-      .then($scope.splitBySex);
+  
+  $scope.selectLocality = function(){
+    $log.log("selectLocality");
+    $log.log($scope.selectedLocality);
+    $scope.loadTrends()
   }
-  */
+
 
 
   $scope.parseTrends = (d) => {
@@ -56,10 +46,25 @@ dataViz.controller('homeController', function (
     return rows
   }
 
+  $scope.parseLocalities = (d) => {
+    $log.log("parseLocalities");
+    let rows = d.rows
+    //$log.log(rows);
+    rows.forEach(function (v) {
+      v.label = v.localidad + " (" + v.total + ")"
+      v.id = v.localidad
+      v.value = v.total
+    });
+    //$log.log(rows);
+    $scope.localities = rows
+    return rows
+  }
+
   $scope.loadTrends = function () {
+    var localityFilter = ( $scope.selectedLocality == "")? "" : "  where localidad = '"+$scope.selectedLocality+"' "
     let query = ` with events as (
       SELECT to_char(event_time, 'YYYY-MM-DD')  as event_day, cartodb_id as id
-      FROM kudosg.accidentes_bta
+      FROM kudosg.accidentes_bta ${localityFilter} 
         )
       , events_agg as ( 
        select event_day, count(id) as total 
@@ -73,12 +78,28 @@ dataViz.controller('homeController', function (
       order by 1 asc `
 
     $log.log(query);
-    $scope.datasetTrends  = d3.json($scope.remoteServiceUrl + query).then($scope.parseTrends);
+    $scope.trends = d3.json($scope.remoteServiceUrl + query).then($scope.parseTrends);
   }
+
+  $scope.loadLocalities = function () {
+    let query = ` SELECT localidad, count(cartodb_id) as total FROM kudosg.accidentes_bta
+    group by localidad order by count(cartodb_id) desc `
+    $log.log(query);
+    $scope.localities = d3.json($scope.remoteServiceUrl + query)
+      .then($scope.parseLocalities).then( (d) => $scope.$apply() )
+  }
+
+  
+
+
+
+
 
   $scope.init = function () {
     $log.log("init - homeController");
+    $scope.loadLocalities();
     $scope.loadTrends();
+
   }
 
   $scope.init();
